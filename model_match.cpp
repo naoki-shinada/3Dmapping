f5c13c7eb9883dd4de0e07541e392d53a5d3d009
naoki-shinada <shina-wase-da@akane.waseda.jp>
2020-05-28 00:39:01 +0900

    モデルと合わせ、Viewerも追加

--- model_match.cpp
+++ model_match.cpp
@@ -7,6 +7,7 @@
 #include <cstdlib> 
 #include <Eigen/Dense>
 #include <math.h>
+#include <pcl/visualization/cloud_viewer.h> 
 
 typedef pcl::PointXYZ PointT;
 using namespace std;
@@ -17,7 +18,7 @@ main(int argc, char** argv)
 {
 	//RANSACパラメータ
 	int  maxitteration = 10000;
-	double r = 0.523;
+	double r = 0.494921;
 	double threshould = 0.03;
 
 	// All the objects needed
@@ -36,14 +37,15 @@ main(int argc, char** argv)
 	pcl::PointIndices::Ptr inliers_plane(new pcl::PointIndices), inliers_cylinder(new pcl::PointIndices);
 
 
-	int i,j;
+	int i, j;
 	string n;
-	ofstream ofs("CppRANSAC.csv");
-	for (i = 0; i < 3; i++) {
+	ofstream ofs("test.csv");
+	for (i = 0; i < 1; i++) {
 		n = to_string(i + 1);
 		// Read in the cloud data
-		reader.read(n + "cylinder2m.pcd", *cloud);
-		
+		//reader.read(n + "cylinder2m.pcd", *cloud);
+		reader.read("stright.pcd", *cloud);
+
 		// Estimate point normals
 		ne.setViewPoint(0.0, 0.0, 1000.0);
 		ne.setSearchMethod(tree);
@@ -57,10 +59,10 @@ main(int argc, char** argv)
 		int itteration = 0;
 		vector<double> bestP(3);
 		vector<double> bestJIKU(3);
-		
+
 		while (itteration < maxitteration) {
-			int a = rand() % cloud->points.size() ;
-			int b = rand() % cloud->points.size() ;
+			int a = rand() % cloud->points.size();
+			int b = rand() % cloud->points.size();
 
 			vector<double> p1 = { cloud->points[a].x, cloud->points[a].y,  cloud->points[a].z };
 			vector<double> p2 = { cloud->points[b].x, cloud->points[b].y,  cloud->points[b].z };
@@ -85,7 +87,7 @@ main(int argc, char** argv)
 				//軸との距離を求める
 				vector<double> v = { x[0] - point1[0],x[1] - point1[1],x[2] - point1[2] };
 				double vsize = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-				double d = sqrt(pow(vsize,2) - pow((v[0] * jiku[0] + v[1] *jiku[1] + v[2] * jiku[2]), 2));
+				double d = sqrt(pow(vsize, 2) - pow((v[0] * jiku[0] + v[1] * jiku[1] + v[2] * jiku[2]), 2));
 				if (d > r - threshould && r + threshould > d) {
 					count++;
 				}
@@ -106,7 +108,44 @@ main(int argc, char** argv)
 		for (j = 0; j < 3; j++) {
 			ofs << bestJIKU[j] << ",";
 		}
-		ofs <<maxinliner<< endl;
+		ofs << maxinliner << endl;
+
+		//モデルの正しさを可視化
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr colored(new pcl::PointCloud<pcl::PointXYZRGB>);
+		colored->points.resize(cloud->points.size());
+		for (j = 0; j < cloud->points.size(); j++) {
+			colored->points[j].x = cloud->points[j].x;
+			colored->points[j].y = cloud->points[j].y;
+			colored->points[j].z = cloud->points[j].z;
+			colored->points[j].r = 0;
+			colored->points[j].g = 0;
+			colored->points[j].b = 255;
+		}
+
+		//vector<double> point1 = { 0.0931,8.16,-0.150 };
+		//vector<double> jiku = { 0.002446,0.999976,0.00207 };
+		vector<double> point1 = bestP;
+		vector<double> jiku = bestJIKU;
+		for (j = 0; j < cloud->points.size(); j++) {
+			vector<double> x = { cloud->points[j].x, cloud->points[j].y,  cloud->points[j].z };
+			//軸との距離を求める
+			vector<double> v = { x[0] - point1[0],x[1] - point1[1],x[2] - point1[2] };
+			double vsize = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+			double d = sqrt(pow(vsize, 2) - pow((v[0] * jiku[0] + v[1] * jiku[1] + v[2] * jiku[2]), 2));
+			if (d > r - threshould && r + threshould > d) {
+				colored->points[j].r = 255;
+				colored->points[j].b = 0;
+			}
+		}
+		pcl::visualization::CloudViewer viewer("Cloud Viewer");
+
+		viewer.showCloud(colored);
+
+		while (!viewer.wasStopped())
+		{
+
+		}
+
 	}
 
 	return (0);
